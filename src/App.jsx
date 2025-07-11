0ee294cd889efae10e3ef29bb0a1850a628fb215
Davi Marques <dmo3@cesar.school>
2025-07-11 14:44:08 -0300

    [Feat] - Adição da Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import ProjectsSection from "./assets/components/ProjectsSection"
-import Footer from "./assets/components/Footer"
-import EmailSection from "./assets/components/EmailSection"
+import { Routes, Route } from "react-router-dom";
+import ProjectsSection from "./assets/components/ProjectsSection";
+import Footer from "./assets/components/Footer";
+import EmailSection from "./assets/components/EmailSection";
+import HeroSection from "./assets/components/HeroSection";
 
 function App() {
-
   return (
     <div className="container mt-24 mx-auto px-12 py-4">
-      <ProjectsSection></ProjectsSection>
-      <Footer></Footer>
-      <EmailSection></EmailSection>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <HeroSection />
+              <ProjectsSection />
+              <Footer />
+              <EmailSection />
+            </>
+          }
+        />
+
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/components/HeroSection.jsx
@@ -0,0 +1,55 @@
+
+import { Link } from "react-router-dom";
+import ReactLogo from "../../assets/vite.svg"
+
+
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start transition-opacity duration-500 opacity-100 scale-100">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I&apos;m{" "}
+            </span>
+            <br />
+          </h1>
+
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptuous.
+          </p>
+
+          <div>
+            <Link
+              to="/"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <Link
+              to="/"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#000000] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div className="col-span-4 place-self-center mt-4 lg:mt-0 transition-opacity duration-500 opacity-100 scale-100">
+          <div className="rounded-full bg-[#ffffff] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <img
+              src={ReactLogo}
+              alt="hero"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[300px] h-[300px] object-cover rounded-full"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
