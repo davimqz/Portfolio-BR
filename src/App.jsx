5b52740169ce07dc18ed4bce96c7d21cba78b8da
Davi Marques <dmo3@cesar.school>
2025-07-11 09:35:45 -0300

    [Feat] - Adição da section de projetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import ProjectsSection from "./assets/components/ProjectsSection"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="container mt-24 mx-auto px-12 py-4">
+      <ProjectsSection></ProjectsSection>
+    </div>
   )
 }
 
