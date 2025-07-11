e68219a9e638f96a57eca095717aa1b26f9e8ee2
Davi Marques <dmo3@cesar.school>
2025-07-11 14:11:58 -0300

    [Feat] - Footer Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import ProjectsSection from "./assets/components/ProjectsSection"
+import Footer from "./assets/components/Footer"
+import EmailSection from "./assets/components/EmailSection"
 
 function App() {
 
   return (
     <div className="container mt-24 mx-auto px-12 py-4">
       <ProjectsSection></ProjectsSection>
+      <Footer></Footer>
+      <EmailSection></EmailSection>
     </div>
   )
 }
